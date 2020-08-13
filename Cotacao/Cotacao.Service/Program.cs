084e7de55166efd069c9bf4a44dadffa9a12ef18
Leonardo Nascimento <leo_overlord@hotmail.com>
2020-08-13 14:22:47 -0300

    Instalação dos nugets iniciais e organização das pastas

---
+++ Cotacao/Cotacao.Service/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cotacao.Service
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
